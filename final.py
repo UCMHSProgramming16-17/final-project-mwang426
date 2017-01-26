a456f5fbcb3c3c39762f09d36910a056eb626e4e
Mandy Wang <mwang@ucvts.org>
2017-01-26 17:12:53 +0000

    line graph

--- final.py
+++ final.py
@@ -1,43 +1,17 @@
-#import csv
+#import csv and pandas as pd
 import csv
-#import pandas as pd
 import pandas as pd
 
-#open the data file and read it
-f = open('crop.csv')
-csv_f = csv.reader(f)
-production = []
-year = []
+data = pd.read_csv('crop.csv')
+data.columns
+data.rename(columns={'APPLES - PRODUCTION, MEASURED IN $  -  <b>VALUE</b>':'Apple Production $'}, inplace=True)
+cleaned_data = pd.DataFrame({'production':data['Apple Production $']})
+cleaned_data.describe()
+cleaned_data = pd.DataFrame({'production':data['Apple Production $'], 'year': data['Year']})
 
-#print production numbers 
-for row in csv_f:
-    print(row[1])
-    production.append(row[18])
-print(production)
+from bokeh.charts import Line, output_file, save, Bar
+l = Line(cleaned_data, x='year', y='production')
+output_file('line.html')
+save(l)
 
-#makes the DataFrame
-df = pd.DataFrame({
-    'APPLES - PRODUCTION, MEASURED IN $  -  <b>VALUE</b>': production
-})
 
-#import line chart commands
-from bokeh.charts import Line, output_file, save
-
-#create and format the chart that is going to be apples_line.html
-Linechart = Line(df, 'APPLES - PRODUCTION, MEASURED IN $  -  <b>VALUE</b>', title = 'Apple Production Line Chart', xlabel = 'year', ylabel = 'Apple production measured in $')
-
-#make the file apples_line.html 
-output_file('apples_line.html')
-save(Linechart)
-
-#import second chart commands, bar graph
-from bokeh.charts import Bar, output_file, save
-
-#create and format the bar graphs so that it is going to be apples_line.html
-Bargraph = Bar(df, 'APPLES - PRODUCTION, MEASURED IN $  -  <b>VALUE</b>', title = 'Apple Proudction Bar Graph', xlabel = 'year', ylabel = 'Apples produced measured in $')
-
-#make the file apples_bar.html
-output_file('apples_bar.html')
-save(Bargraph)
-
-#import third chart commands, step graph
